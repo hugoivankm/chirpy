7e7a2e0af6261182d59d3817c1a0beba61a78429
hugoivankm <hugoivank@gmail.com>
2026-04-27 22:52:55 -0600

    update linter config

--- main.go
+++ main.go
@@ -58,19 +58,27 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK\n"))
+	_, err := w.Write([]byte("OK\n"))
+	if err != nil {
+		respondWithError(w, http.StatusServiceUnavailable, "Service Unavailable")
+	}
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`
+	_, err := w.Write([]byte(fmt.Sprintf(`
 	<html>
   	  <body>
         <h1>Welcome, Chirpy Admin</h1>
         <p>Chirpy has been visited %d times!</p>
      </body>
     </html>`, cfg.fileserverHits.Load())))
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
@@ -89,7 +97,12 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	cfg.fileserverHits.Store(0)
-	w.Write(fmt.Appendf(nil, "Hits: %d\n", 0))
+	_, err = w.Write(fmt.Appendf(nil, "Hits: %d\n", 0))
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
 
 }
 
@@ -212,7 +225,10 @@ func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) 
 }
 
 func main() {
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Unable to load environment")
+	}
 
 	dbURL := os.Getenv("DB_URL")
 	platform := os.Getenv("PLATFORM")
